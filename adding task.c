a882080902c94f57e44fb3fb23a527dee9c5564d
Ramakrishnasiripalli <92074723+Ramakrishnasiripalli@users.noreply.github.com>
2021-10-23 10:59:36 +0530

    Add files via upload

---
+++ adding task.c
@@ -0,0 +1,21 @@
+/*Additing two poin values*/
+#include<stdio.h>
+void main()
+{
+	
+	// variable declaratio
+	float number1, number2, sum;
+	
+	//initilization
+	number1 = 9.5;
+	number2 = 9.7;
+	
+	// calculation
+	sum = number1+number2; //9.5+9.7 = 19.2
+	
+	// output
+	printf("sum of %lf and %lf is %lf",number1,number2,sum);
+	
+	
+	
+}
