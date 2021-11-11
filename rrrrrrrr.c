1d2e0235679a4b023edc1df1c745abe0c5e43954
Ramakrishnasiripalli <92074723+Ramakrishnasiripalli@users.noreply.github.com>
2021-11-11 10:48:00 +0530

    Add files via upload

---
+++ rrrrrrrr.c
@@ -0,0 +1,13 @@
+// program to print square of number
+#include<stdio.h>
+void main()
+
+{
+int side,area,perimeter;
+printf("enter side length");
+scanf("%d", &side);
+area = side*side;
+perimeter = 4*side;
+printf("area of square is %d\n",area);
+printf("perimeter of square is %d",perimeter);
+}
